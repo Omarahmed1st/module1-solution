baa6f043a8132281a3cae0e9271fa8872e262f46
Omarahmed1st <140261654+Omarahmed1st@users.noreply.github.com>
2024-08-13 18:34:02 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.lunchItems = '';
+        $scope.message = '';
+        $scope.messageClass = '';
+        $scope.borderClass = '';
+
+        $scope.checkIfTooMuch = function () {
+            if (!$scope.lunchItems.trim()) {
+                $scope.message = "Please enter data first";
+                $scope.messageClass = 'red';
+                $scope.borderClass = 'red';
+                return;
+            }
+
+            var items = $scope.lunchItems.split(',').filter(function(item) {
+                return item.trim() !== '';
+            });
+
+            if (items.length <= 3) {
+                $scope.message = "Enjoy!";
+                $scope.messageClass = 'green';
+                $scope.borderClass = 'green';
+            } else {
+                $scope.message = "Too much!";
+                $scope.messageClass = 'green';
+                $scope.borderClass = 'green';
+            }
+        };
+    }
+})();
